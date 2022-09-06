0818b2a948aaebbc1492f8852d4d3c5a4c1abdf1
Carlo Goretti <carlo.goretti@live.se>
2022-09-06 15:27:29 +0200

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { NavLink as Link } from 'react-router-dom'
 import styled from 'styled-components'
-import '../css/nav.css'
 import { AppRoutes } from '../routes'
 import { FaUserCircle } from 'react-icons/fa'
 import AutoComplete from './AutoComplete'
