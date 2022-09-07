dd79e5c5f894e41db93e255c0b43b27745d12d68
no92one <linus.lindroth.92@gmail.com>
2022-09-07 10:25:06 +0200

    added frontend folder in to backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+import './css/App.css'
+import Nav from './components/Nav'
+import {
+  BrowserRouter,
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
+import { AppRoutes } from './routes';
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className='container'>
+      <Nav />
+      <Routes>
+        {AppRoutes.map((item, index) => <Route key={index} path={item.path} element={item.component} />)}
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App
