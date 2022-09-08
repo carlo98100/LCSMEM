7b8be7c6be60d09dc8d97859f952885f335ae50d
Chiltrin <90396309+Chiltrin@users.noreply.github.com>
2022-09-08 17:54:08 +0200

    .

--- frontend/src/views/PurchaseHistory.jsx
+++ frontend/src/views/PurchaseHistory.jsx
@@ -1,10 +1,60 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
 import styled from "styled-components";
-import Input from "../components/Input";
+import { Link } from "react-router-dom";
 
 function PurchaseHistory() {
-  return <div>Purchase History</div>;
+  return (
+    <Container>
+      <LeftContainer>
+        <Text>Your profile</Text>
+        <Link to="/PurchaseHistory">Purchase History</Link>
+        <Link to="/UserDetails">User Details</Link>
+      </LeftContainer>
+      <RightContainer>
+        <TicketsTitle>Your purchased tickets</TicketsTitle>
+      </RightContainer>
+    </Container>
+  );
 }
 
 export default PurchaseHistory;
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const LeftContainer = styled.div`
+  background-color: #ececec;
+  flex: 1 1 30%;
+  min-width: 15ch;
+  height: calc(100vh - 70px);
+`;
+
+const RightContainer = styled.div`
+  background-color: #b96798;
+  flex: 1 1 70%;
+  // ch = how many characters wide
+  min-width: 25ch;
+  height: calc(100vh - 70px);
+`;
+
+const Text = styled.h1`
+  text-align: center;
+  background-color: gray;
+  padding: 15px;
+  border-radius: 10px;
+`;
+
+const TextH3 = styled.h3`
+  text-align: center;
+  background-color: gray;
+  padding: 15px;
+  border-radius: 10px;
+`;
+
+const TicketsTitle = styled.h1`
+  text-align: center;
+  background-color: gray;
+  padding: 15px;
+  margin: 0;
+`;
--- frontend/src/views/UserDetails.jsx
+++ frontend/src/views/UserDetails.jsx
@@ -4,7 +4,58 @@ import styled from "styled-components";
 import Input from "../components/Input";
 
 function UserDetails() {
-  return <div>User Details</div>;
+  return (
+    <Container>
+      <LeftContainer>
+        <Text>Your profile</Text>
+        <Link to="/PurchaseHistory">Purchase History</Link>
+        <Link to="/UserDetails">User Details</Link>
+      </LeftContainer>
+      <RightContainer>
+        <TicketsTitle>Your information</TicketsTitle>
+      </RightContainer>
+    </Container>
+  );
 }
 
 export default UserDetails;
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const LeftContainer = styled.div`
+  background-color: #ececec;
+  flex: 1 1 30%;
+  min-width: 15ch;
+  height: calc(100vh - 70px);
+`;
+
+const RightContainer = styled.div`
+  background-color: #67b98d;
+  flex: 1 1 70%;
+  // ch = how many characters wide
+  min-width: 25ch;
+  height: calc(100vh - 70px);
+`;
+
+const Text = styled.h1`
+  text-align: center;
+  background-color: gray;
+  padding: 15px;
+  border-radius: 10px;
+`;
+
+const TextH3 = styled.h3`
+  text-align: center;
+  background-color: gray;
+  padding: 15px;
+  border-radius: 10px;
+`;
+
+const TicketsTitle = styled.h1`
+  text-align: center;
+  background-color: gray;
+  padding: 15px;
+  margin: 0;
+`;
