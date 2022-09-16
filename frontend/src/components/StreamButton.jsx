add595cba0e89bcb75bd7270598d6f62ea915b1f
no92one <linus.lindroth.92@gmail.com>
2022-09-16 07:56:42 +0200

    updated StreamButton to take VideoId param

--- frontend/src/components/StreamButton.jsx
+++ frontend/src/components/StreamButton.jsx
@@ -1,29 +1,36 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import {  useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
-const StreamButton = (videoId) => {
+function StreamButton({ VideoId }) {
 
-    const [videos, setVideos] = useState([])
-
-    useEffect(() => {
-        async function getVideos() {
-            let videoList = await fetch('/data/videos')
-            videoList = await videoList.json()
-            console.log(videoList)
-            setVideos(videoList)
-        }
-        getVideos()
-    }, [])
+    const navigate = useNavigate();
 
     return <>
-        <videoLinks>
-            {videos.map(video => <a href={`/Stream/${video.id}`}>{video.name}</a>)}
-        </videoLinks>
+        <VideoLink>
+            <button onClick={() => navigate(`/Stream/${VideoId}`)}>Stream</button>
+        </VideoLink>
     </>
 }
 
 export default StreamButton
 
-const videoLinks = styled.div`
-color: black;
+const VideoLink = styled.div`
+button {
+    background: black;
+    color: white;
+    border: 2px solid;
+    border-color: black;
+    padding: 1em 2em;
+    font-size: 1em;
+    transform: scale(0.8);
+    transition: color 0.2s, border-color 0.2s, box-shadow 0.2s, transform 0.2s;
+    &:hover {
+        border-color: red;
+        color: red;
+        box-shadow: 0 0.5em 0.5em -0.4em black;
+        transform: scale(1) translateY(-0.2em);
+        cursor: pointer;
+    }
+}
 `
