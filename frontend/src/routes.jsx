0193ae17931698c3cdfe10fc520b9048dbd4f938
erikenander95 <erik.enander95@gmail.com>
2022-09-16 14:26:41 +0200

    added button for redirect to stripe

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -9,86 +9,93 @@ import YourTickets from "./views/YourTickets";
 import ArtistPage from "./views/ArtistPage";
 import Stream from "./views/Stream";
 import EventPage from "./views/eventPage";
-
+import Checkout from "./views/Checkout";
 
 export const AppRoutes = [
-	{
-		name: "Home",
-		path: "/",
-		display: true,
-		component: <Home />,
-	},
-	{
-		name: "Link1",
-		path: "/link-1",
-		display: true,
-		component: <Home />,
-	},
-	{
-		name: "Link2",
-		path: "/link-2",
-		display: true,
-		component: <Home />,
-	},
-	{
-		name: "ArtistPage",
-		path: "/ArtistPage/:artistId",
-		display: true,
-		component: <ArtistPage />,
-	},
-	{
-		name: "Login",
-		path: "/login",
-		display: false,
-		component: <Login />,
-	},
-	,
-	{
-		name: "Signup",
-		path: "/signup",
-		display: false,
-		component: <Signup />,
-	},
+  {
+    name: "Home",
+    path: "/",
+    display: true,
+    component: <Home />,
+  },
+  {
+    name: "Link1",
+    path: "/link-1",
+    display: true,
+    component: <Home />,
+  },
+  {
+    name: "Link2",
+    path: "/link-2",
+    display: true,
+    component: <Home />,
+  },
+  {
+    name: "ArtistPage",
+    path: "/ArtistPage/:artistId",
+    display: true,
+    component: <ArtistPage />,
+  },
+  {
+    name: "Login",
+    path: "/login",
+    display: false,
+    component: <Login />,
+  },
+  ,
+  {
+    name: "Signup",
+    path: "/signup",
+    display: false,
+    component: <Signup />,
+  },
 
-	{
-		name: "ProfilePage",
-		path: "/ProfilePage",
-		display: false,
-		component: <ProfilePage />,
-	},
-	{
-		name: "PurchaseHistory",
-		path: "/PurchaseHistory",
-		display: false,
-		component: <PurchaseHistory />,
-	},
-	{
-		name: "UserDetails",
-		path: "/UserDetails",
-		display: false,
-		component: <UserDetails />,
-	},
-	{
-		name: "TicketInfo",
-		path: "/TicketInfo",
-		display: false,
-		component: <YourTickets />,
-	},
-	{
-		name: "Pick Stream",
-		path: "/PickStream",
-		display: true,
-		component: <PickStream />,
-	}, {
-		name: "Stream",
-		path: "/Stream/:id",
-		display: false,
-		component: <Stream />,
-	},
-	{
-		name: "Event page",
-		path: "/EventPage",
-		display: true,
-		component: <EventPage />,
-	},
+  {
+    name: "ProfilePage",
+    path: "/ProfilePage",
+    display: false,
+    component: <ProfilePage />,
+  },
+  {
+    name: "PurchaseHistory",
+    path: "/PurchaseHistory",
+    display: false,
+    component: <PurchaseHistory />,
+  },
+  {
+    name: "UserDetails",
+    path: "/UserDetails",
+    display: false,
+    component: <UserDetails />,
+  },
+  {
+    name: "TicketInfo",
+    path: "/TicketInfo",
+    display: false,
+    component: <YourTickets />,
+  },
+  {
+    name: "Pick Stream",
+    path: "/PickStream",
+    display: true,
+    component: <PickStream />,
+  },
+  {
+    name: "Stream",
+    path: "/Stream/:id",
+    display: false,
+    component: <Stream />,
+  },
+  {
+    name: "Event page",
+    path: "/EventPage",
+    display: true,
+    component: <EventPage />,
+  },
+  {
+    name: "Checkout",
+    path: "/Checkout",
+    display: true,
+    component: <Checkout />,
+  },
 ];
---
+++ frontend/src/views/Checkout.jsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import React from "react";
+import { getCart } from "../CartUtils";
+
+function Checkout() {
+  const tickets = getCart();
+  const handleClick = async () => {
+    await fetch(`/data/checkout`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(tickets),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        window.location.replace(data.url);
+      })
+      .catch((err) => console.error(err));
+  };
+  console.log(tickets);
+  return (
+    <div>
+      {tickets.items?.map((item, index) => {
+        return <h1>{item.description}</h1>;
+      })}
+      <div>
+        <button type="button" onClick={handleClick}>
+          Poceed to purchase
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Checkout;
