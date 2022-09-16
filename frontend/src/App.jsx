aa5bb1ac6a30d17b0bc83b79658f3c051ea2bd21
no92one <linus.lindroth.92@gmail.com>
2022-09-16 06:46:12 +0200

    added VideoList to contexts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,20 +5,23 @@ import { AppRoutes } from "./routes";
 import UserContextProvider from "./contexts/UserContext";
 import { EventContextProvider } from "./contexts/eventList";
 import { ArtistContextProvider } from "./contexts/artistList";
+import { VideoContextProvider } from "./contexts/VideoList";
 
 function App() {
   return (
     <UserContextProvider>
       <EventContextProvider>
         <ArtistContextProvider>
-          <div className="container">
-            <Nav />
-            <Routes>
-              {AppRoutes.map((item, index) => (
-                <Route key={index} path={item.path} element={item.component} />
-              ))}
-            </Routes>
-          </div>
+          <VideoContextProvider>
+            <div className="container">
+              <Nav />
+              <Routes>
+                {AppRoutes.map((item, index) => (
+                  <Route key={index} path={item.path} element={item.component} />
+                ))}
+              </Routes>
+            </div>
+          </VideoContextProvider>
         </ArtistContextProvider>
       </EventContextProvider>
     </UserContextProvider>
--- frontend/src/components/StreamButton.jsx
+++ frontend/src/components/StreamButton.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import styled from "styled-components";
 
-const StreamButton = () => {
+const StreamButton = (videoId) => {
 
     const [videos, setVideos] = useState([])
 
