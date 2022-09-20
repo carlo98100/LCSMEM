62563d3c2dab18da956d6cca9a3cf91e0bb9e3f5
Chiltrin <90396309+Chiltrin@users.noreply.github.com>
2022-09-20 13:23:22 +0200

    made working carusel

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
+import { useContext } from "react";
+import "react-responsive-carousel/lib/styles/carousel.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import ArtistContext from "../contexts/artistList";
+import styled from "styled-components";
 
 function Home() {
-	return <div>Home</div>;
+  const { artist } = useContext(ArtistContext);
+
+  const imageList = [];
+
+  function getImages() {
+    artist.map((artist) => {
+      imageList.push(artist.Image);
+    });
+    console.log(imageList);
+    return imageList;
+  }
+
+  return (
+    <Container>
+      <Carousel>
+        {getImages().map((image) => (
+          <div key={image}>
+            <img src={image} />
+          </div>
+        ))}
+      </Carousel>
+    </Container>
+  );
 }
 
 export default Home;
+
+const Container = styled.div`
+  display: flex;
+`;
