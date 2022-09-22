15671489c22d515da6802e3919414bd66fa3376f
Carlo Goretti <carlo.goretti@live.se>
2022-09-22 19:36:35 +0200

    Update ConfirmOrderPage.jsx

--- frontend/src/views/ConfirmOrderPage.jsx
+++ frontend/src/views/ConfirmOrderPage.jsx
@@ -5,36 +5,6 @@ import { useParams } from "react-router-dom";
 import { getCart } from "../CartUtils";
 
 function ConfirmOrderPage() {
-	// const { sessionId } = useParams();
-	// const [checkout, setCheckout] = useState(null);
-
-	// useEffect(() => {
-	// 	fetch(`/data/checkout/${sessionId}`, () => {})
-	// 		.then((response) => response.json())
-	// 		.then((data) => {
-	// 			setCheckout(data);
-	// 			fetch("/data/order/create", {
-	// 				method: "POST",
-	// 				headers: {
-	// 					"Content-Type": "application/json",
-	// 				},
-	// 				body: JSON.stringify({
-	// 					checkout: data,
-	// 					orderItems: getCart,
-	// 				}), // body data type must match "Content-Type" header
-	// 			})
-	// 				.then((response) => {
-	// 					if (response.status == 403) {
-	// 						isSignedIn();
-	// 					} else {
-	// 						response.json();
-	// 					}
-	// 				})
-	// 				.then((data) => setUser({ email: form.email, loggedIn: data.loggedIn }))
-	// 				.catch((err) => console.error(err));
-	// 		})
-	// 		.catch((err) => console.error(err));
-	// }, []);
 
 	return <div>Your order is comfirmed</div>;
 }
